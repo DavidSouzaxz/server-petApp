af071b1d2a076ec89dede992060f5099aef07901
DavidSouzaxz <daviddavi1221@gmail.com>
2026-04-12 15:19:10 -0300

    Init: Commit inicial do projeto com o projeto inicial
    Signed-off-by: DavidSouzaxz <daviddavi1221@gmail.com>

---
+++ src/main/java/com/project/pettvaccine_api/PettvaccineApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.pettvaccine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettvaccineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettvaccineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pettvaccine_api/PettvaccineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pettvaccine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettvaccineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
