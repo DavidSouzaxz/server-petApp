156fa2356d6bbeb002ca706f70d17811dcb87f90
DavidSouzaxz <daviddavi1221@gmail.com>
2026-04-12 18:15:27 -0300

    Feat: Created Repositoies

---
+++ src/main/java/com/project/pettvaccine_api/repositories/PetRepository.java
@@ -0,0 +1,20 @@
+package com.project.pettvaccine_api.repositories;
+
+import com.project.pettvaccine_api.entity.PetsEntity;
+import com.project.pettvaccine_api.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PetRepository extends JpaRepository<PetsEntity, UUID> {
+
+   List<PetsEntity> findByBreed(String breed);
+   List<PetsEntity> findByBirthDate(LocalDate date);
+
+
+}
---
+++ src/main/java/com/project/pettvaccine_api/repositories/VaccinesRepository.java
@@ -0,0 +1,16 @@
+package com.project.pettvaccine_api.repositories;
+
+import com.project.pettvaccine_api.entity.VaccinesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface VaccinesRepository extends JpaRepository<VaccinesEntity, UUID> {
+    Optional<VaccinesEntity> findByName(String name);
+    List<VaccinesEntity> findByPetId(UUID petId);
+}
