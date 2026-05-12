9c0cc61a01050793014456227ae56d5153c84faa
DavidSouzaxz <daviddavi1221@gmail.com>
2026-05-12 08:15:11 -0300

    Fix: ajusted requisition header
    Signed-off-by: DavidSouzaxz <daviddavi1221@gmail.com>

--- src/main/java/com/project/pettvaccine_api/controllers/HealthController.java
+++ src/main/java/com/project/pettvaccine_api/controllers/HealthController.java
@@ -1,16 +1,24 @@
 package com.project.pettvaccine_api.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/sys")
 public class HealthController {
+    @Value("${HEALTH_KEY}") // O Spring busca automaticamente nas Env Vars do Render
+    private String secretKey;
 
     @GetMapping("/sync")
-    public ResponseEntity<Void> heartbeat() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> heartbeat(@RequestHeader("X-Health-Key") String receivedKey) {
+        if (secretKey.equals(receivedKey)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
